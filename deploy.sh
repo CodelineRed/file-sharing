011a8072f2a90886a1595ac971c53a1621fb6b0c
InsanityMeetsHH <software@insanitymeetshh.net>
2018-09-16 18:49:36 +0200

    Added paragonie/random_compat version restriction. Changed deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 git status
+git checkout production
 git pull
 
-php ../composer.phar install
+# Install all or some packages
+php composer update
+
+# Add changes from entities to database
 php doctrine orm:schema-tool:update --force
