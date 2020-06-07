cd0de54cd34daca75233d86c516d9650cb47ac96
InsanityMeetsHH <software@insanitymeetshh.net>
2020-06-07 11:56:51 +0200

    Changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git checkout production
 git pull
 
 # Install all or some packages
-/usr/bin/php7.1-cli ../composer.phar install
+/usr/bin/php7.4-cli ../composer.phar install
 
 # Add changes from entities to database
-/usr/bin/php7.1-cli doctrine orm:schema-tool:update --force
+/usr/bin/php7.4-cli doctrine orm:schema-tool:update --force
